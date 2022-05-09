4e77c7500d4e7b48efbc5042d31c7d440b07cc76
ogbanugot <ogbanugot@gmail.com>
2022-05-09 16:46:25 +0100

    add util

--- disq.go
+++ disq.go
@@ -2,10 +2,7 @@ package disq
 
 import (
 	"log"
-	"math/rand"
 	"os"
-	"strconv"
-	"time"
 )
 
 var Logger *log.Logger
@@ -16,15 +13,4 @@ func init() {
 
 func SetLogger(logger *log.Logger) {
 	Logger = logger
-}
-
-func UnixMs(tm time.Time) int64 {
-	return tm.UnixNano() / int64(time.Millisecond)
-}
-
-func ConsumerName() string {
-	s, _ := os.Hostname()
-	s += ":pid:" + strconv.Itoa(os.Getpid())
-	s += ":" + strconv.Itoa(rand.Int())
-	return s
 }
